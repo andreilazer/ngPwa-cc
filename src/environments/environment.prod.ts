884f8d5b017f1916384756e3c19b04190d5f27e3
Andrei Lazar <andrei.lazar@centric.eu>
2020-01-29 13:47:39 +0200

    update environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    "DATA_API_URL": "https://angularpwaserver.azurewebsites.net",
+    "PUSH_API_URL": "",
+    "VAPID_PUBLIC_KEY": ""
+  }
 };

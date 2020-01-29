0af85100643966f935aa773528730129b021ec38
Andrei Lazar <andrei.lazar@centric.eu>
2020-01-29 13:45:59 +0200

    added pwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    "DATA_API_URL": "https://angularpwaserver.azurewebsites.net",
+    "PUSH_API_URL": "http://localhost:3000",
+    "VAPID_PUBLIC_KEY": "BM88mSlUg4mvjcPK5QrzRfQzow91F47iEazCnoTBQ8Hv_AVrJviLcnrNumTK319qWOt43sgOzBJs6UrdOW5IxHg"
+  }
 };

f43e04350adda1795550a25b06392aefda416951
Andrei Lazar <andrei.lazar@centric.eu>
2020-02-01 18:13:10 +0200

    add install button

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -18,6 +18,10 @@ export class NavigationComponent implements OnDestroy {
   destroy = new Subject();
   online = this.updateService.online;
 
+  installPromptEvent;
+  btnInstallDisabled = true;
+
+
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(map(result => result.matches));
@@ -29,6 +33,34 @@ export class NavigationComponent implements OnDestroy {
   ) {
     // uncomment this line to check for app updates every 30 seconds
     // this.subscribeToUpdates();
+    this.prepareInstallButton();
+  }
+
+  install() {
+    this.btnInstallDisabled = true;
+    // Show the modal add to home screen dialog
+    this.installPromptEvent.prompt();
+    // Wait for the user to respond to the prompt
+    this.installPromptEvent.userChoice.then(choice => {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      // Clear the saved prompt since it can't be used again
+      this.installPromptEvent = null;
+    });
+  }
+
+  private prepareInstallButton() {
+    window.addEventListener('beforeinstallprompt', event => {
+      // Prevent Chrome <= 67 from automatically showing the prompt
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.installPromptEvent = event;
+      // Update the install UI to notify the user app can be installed
+      this.btnInstallDisabled = false;
+    });
   }
 
   private subscribeToUpdates() {
